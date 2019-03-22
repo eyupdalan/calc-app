de820e19a35bce7524e461c943eee83ee454196a
Edalan <Edalan@ziraatteknoloji.com>
2019-03-22 13:02:56 +0300

    eyup adds changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
 import {Provider} from "react-redux";
 import configureStore from "./redux/configureStore";
 
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-
-// import Menu from "./components/Menu";
-// import Game from "./components/Game";
 
 import Game from "./components/Game2";
 
@@ -16,12 +11,6 @@ class App extends Component {
 		return (
 			<Provider store={configureStore()}>
 				<Game/>
-				{/*<Router>*/}
-					{/*<div className="App">*/}
-						{/*<Route path="/" exact component={Menu}/>*/}
-						{/*<Route path="/game/" component={Game}/>*/}
-					{/*</div>*/}
-				{/*</Router>*/}
 			</Provider>
 		);
 	}
